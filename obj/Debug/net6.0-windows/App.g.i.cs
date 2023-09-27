dc25719f83026215a17ff97816a1078498d852cd
Hedgie <118125119+HungryHedgie@users.noreply.github.com>
2023-09-27 09:03:38 +0100

    Experimented with Anti-Aliasing

--- obj/Debug/net6.0-windows/App.g.i.cs
+++ obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace Modelling___Computing_NEA {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Modelling___Computing_NEA {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
         public static void Main() {
             Modelling___Computing_NEA.App app = new Modelling___Computing_NEA.App();
             app.InitializeComponent();
